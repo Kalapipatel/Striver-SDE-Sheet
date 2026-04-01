2a9283db31ed36f1ff8edacce466276f5adc17dd
Kalapi Patel <103921193+Kalapipatel@users.noreply.github.com>
2026-04-01 17:05:09 +0530

    Create 01_Rotate matrix by 90 degrees.java

---
+++ Array-2/01_Rotate matrix by 90 degrees.java
@@ -0,0 +1,64 @@
+/*
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+Example 2:
+
+
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ 
+
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000
+*/
+
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
+
+/*
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+*/
+
+class Solution {
+    public void rotate(int[][] arr) {
+        int n = arr.length;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n/2; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[i][n-1-j];
+                arr[i][n-1-j] = temp;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-1-i; j++){
+                if(i + j == n-1) continue;
+                // System.out.println(arr[i][j] + " " + arr[n-1-j][n-1-i]);
+                int temp = arr[i][j];
+                arr[i][j] = arr[n-1-j][n-1-i];
+                arr[n-1-j][n-1-i] = temp;
+            }
+        }
+
+    }
+}
